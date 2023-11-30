ba07e3fa0f330f2eb59fe43d35f0272cfcbeef13
“Nabin <nabinkandel@student.pu.edu.np>
2023-11-30 20:09:37 +0545

    update Row.jsx

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -15,15 +15,16 @@ const Row = ({ title, fetchURL   }) => {
     console.log(fetchURL)
 
   return (
-    <><h2 className='teaxt-white font-bold p-4 md:text-xl' >{title}  </h2>
-        <div className='  relative flex items-center'>
-          <MdChevronLeft className=" bg-white rounded-full text-black absolute  opacity-50" size={40}/>
+    <><h2 className='teaxt-white font-bold p-4 md:text-xl ' >{title}  </h2>
+        <div className='flex items-center relative group'>
+          <MdChevronLeft className=" bg-white rounded-full text-black absolute  left-1  opacity-50 cursor-pointer z-10 hidden group-hover:block" size={40}/>
+
             <div className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative' id={'slider'}>      
                     {movies.map((item, id) => (
                 <Movie item={item}/>
                 ))}
             </div>
-            <MdChevronRight className=" bg-white rounded-full text-black absolute  opacity-50  " size={40}/>
+            <MdChevronRight className="bg-white rounded-full text-black absolute right-1  opacity-50 cursor-pointer z-10 hidden group-hover:block" size={40}/>
         </div>
     </>
     
