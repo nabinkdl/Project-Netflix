968518d2788481d6266d0ddb6a61fc38a67d2e15
“Nabin <nabinkandel@student.pu.edu.np>
2023-11-30 18:07:44 +0545

    feat: :sparkles:

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from "react"
+
 import axios from "axios"
-const Row = ({ title, fetchURL ,  }) => {
+import Movie from "./Movie";
+const Row = ({ title, fetchURL   }) => {
    
     const [movies, setMovies] = useState([]);
+
 
     useEffect(() => {
         axios.get(fetchURL).then((response) => {
@@ -12,18 +15,12 @@ const Row = ({ title, fetchURL ,  }) => {
     console.log(fetchURL)
 
   return (
-    <><h2 className='teaxt-white font-bold p-4 md:text-xl' >{title}</h2>
+    <><h2 className='teaxt-white font-bold p-4 md:text-xl' >{title}  </h2>
         <div className=' relative flex items-center'>
-            <div className="" id={'slider'}>      
-            
-            {movies.map((item, id) => (
-           <div className=" w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 ">
-            <img className=" object-cover w-full h-auto block"  src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}/>
-            <div className=" absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 text-white hover:opacity-100">
-                    <p className="className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{item?.title}</p>
-            </div>
-           </div>
-          ))}
+            <div className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative' id={'slider'}>      
+                    {movies.map((item, id) => (
+                <Movie item={item}/>
+                ))}
             </div>
         </div>
     </>
